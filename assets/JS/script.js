ef6cc6ba9c631aa0ba814dcce3c7d1e003121b7e
Nakisa Alipour <n.alipour777@yahoo.com>
2023-03-21 14:14:42 +1100

    Update script.js
    Display the current day at the top of the calendar when the page is loaded

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+/* The following function wraps all of the code that interacts with the DOM, 
+ensuring that it is executed only when the page has finished loading. */
+
+$(document).ready(function() {
+  //Display the current dau at the top of the calender
+  var currentDate = dayjs().format("dddd, MMMM D");
+  $('#currentDay').text(currentDate);
+})
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +32,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
